5864951fcfa41d50a5ccfc5541a8dba67b9eea45
android <outthinkingandroid@gmail.com>
2025-02-28 15:55:31 +0530

    bug fix in article class

--- app/src/main/java/com/outthinking/newsmvvmex/models/Article.kt
+++ app/src/main/java/com/outthinking/newsmvvmex/models/Article.kt
@@ -21,4 +21,31 @@ data class Article(
     val title: String,
     val url: String,
     val urlToImage: String
-):Serializable
+):Serializable{
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        if (content.isEmpty()) {
+            result = 31 * result + content.hashCode()
+        }
+        return result
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Article
+
+        if (id != other.id) return false
+        if (author != other.author) return false
+        if (content != other.content) return false
+        if (description != other.description) return false
+        if (publishedAt != other.publishedAt) return false
+        if (source != other.source) return false
+        if (title != other.title) return false
+        if (url != other.url) return false
+        if (urlToImage != other.urlToImage) return false
+
+        return true
+    }
+}
